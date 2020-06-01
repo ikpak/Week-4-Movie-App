e186a3f1bbc39fdced7d65d271496fe647b9c7ff
Tran Hai Bao <tranhaibao1997@gmail.com>
2020-06-01 15:59:13 +0700

    add Bao code

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -9,14 +9,11 @@ export default function MovieList(props) {
         return <div>Loading...</div>
     }
 
-    function moveDataToHigher(id) {
-        props.moveDataToHigher(id)
-    }
 
     return (
         <Row>
             {props.movieList.map(item => {
-                return <Col className="cards" lg="3" xs="12"><MovieCard movie={item} genresFromMovieList={props.genresFromApp} openModal={props.openModal} moveDataToHigher={moveDataToHigher} /></Col>
+                return <Col className="cards" lg="3" xs="12"><MovieCard movie={item} genresFromMovieList={props.genresFromApp} openModal={props.openModal}  /></Col>
             })}
         </Row>
     )
